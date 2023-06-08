2b82b9f0c5fd177630bea74305467a95a1e4e8bb
An jaegyeong <anjaekk@gmail.com>
2023-06-08 21:41:22 +0900

    add: cors middleware

--- src/envs.py
+++ src/envs.py
@@ -4,5 +4,9 @@ from os import getenv
 
 envs = {
     "DATABASE_URL": getenv("DATABASE_URL"),
+    "ALLOW_ORIGINS": getenv("ALLOW_ORIGINS"),
+    "ALLOW_METHODS": getenv("ALLOW_METHODS"),
+    "ALLOW_HEADERS": getenv("ALLOW_HEADERS"),
+    "ALLOW_CREDENTIALS": getenv("ALLOW_CREDENTIALS"),
 }
 config = argparse.Namespace(**envs)
